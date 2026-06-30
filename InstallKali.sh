dbc12e994e09b31591c9570ceb0cc1eb75144951
Windows10Pro <buitan2611981tan@gmail.com>
2026-06-30 07:45:10 +0700

    Update InstallKali.sh

--- InstallKali.sh
+++ InstallKali.sh
@@ -19,25 +19,55 @@ if [ "$skip" != 1 ]; then
 	fi
 	cur=$(pwd)
 	mkdir -p "$folder"
-	echo -e "\e[32m[*] \e[34mDecompressing RootFS (vui lòng chờ)..."
-	proot --link2symlink tar -xf ${cur}/${tarball} -C "$folder" || (echo -e "\e[91mFailed to decompress RootFS!"; exit 1)
+	cd "$folder"
+	echo -e "\e[32m[*] \e[34mDecompressing RootFS..."
+	proot --link2symlink tar -xf ${cur}/${tarball} || (echo -e "\e[91mFailed to decompress RootFS!"; echo; exit 1)
+	cd "$cur"
 fi
-
 mkdir -p kali-binds
 bin="start-kali.sh"
 echo -e "\e[32m[*] \e[34mCreating startup script..."
 cat > $bin <<- EOM
 #!/data/data/com.termux/files/usr/bin/bash
-cd \$(dirname \$0)
-unset LD_PRELOAD
-command="proot --link2symlink -0 -r $folder"
-command+=" -b /dev -b /proc -b /sys -b /sdcard"
-command+=" -w /root /usr/bin/env -i HOME=/root PATH=/usr/local/sbin:/usr/local/bin:/bin:/usr/bin:/sbin:/usr/sbin TERM=\$TERM LANG=C.UTF-8 /bin/bash --login"
-\$command
-EOM
 
+cd \$(dirname \$0)
+## unset LD_PRELOAD in case termux-exec is installed
+unset LD_PRELOAD
+command="proot"
+command+=" --link2symlink"
+command+=" -0"
+command+=" -r $folder"
+if [ -n "\$(ls -A kali-binds)" ]; then
+    for f in kali-binds/* ;do
+      . \$f
+    done
+fi
+command+=" -b /dev"
+command+=" -b /proc"
+command+=" -b kali-fs/tmp:/dev/shm"
+## uncomment the following line to have access to the home directory of termux
+#command+=" -b /data/data/com.termux/files/home:/root"
+## uncomment the following line to mount /sdcard directly to / 
+command+=" -b /sdcard"
+command+=" -w /root"
+command+=" /usr/bin/env -i"
+command+=" HOME=/root"
+command+=" PATH=/usr/local/sbin:/usr/local/bin:/bin:/usr/bin:/sbin:/usr/sbin:/usr/games:/usr/local/games"
+command+=" TERM=\$TERM"
+command+=" LANG=C.UTF-8"
+command+=" /bin/bash --login"
+com="\$@"
+if [ -z "\$1" ];then
+    exec \$command
+else
+    \$command -c "\$com"
+fi
+EOM
+echo -e "\e[32m[*] \e[34mConfiguring Shebang..."
 termux-fix-shebang $bin
+echo -e "\e[32m[*] \e[34mSetting execution permissions..."
 chmod +x $bin
+echo -e "\e[32m[*] \e[34mRemoving RootFS image..."
 rm -rf $tarball
 echo
 echo -e "\e[32mKali Linux was successfully installed!\e[39m"
